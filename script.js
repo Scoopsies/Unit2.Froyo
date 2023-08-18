314d4b8e369b83ba816c22b605a97062e38c18c6
Scoopsies <mooninites103@gmail.com>
2023-08-18 16:38:35 -0500

    Completed the assignment

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello World")
+const input = window.prompt("Welcome to cone 'lickin, what ice cream you pickin", "vanilla,vanilla,vanilla,strawberry,coffee,coffee" );
+const arr = input.split(",");
+console.log(arr)
+let obj = {};
+
+
+for (let i = 0; i < arr.length; i++){
+    let loop = arr[i];
+    if (obj[loop] === undefined){
+        obj[loop] = 0;
+    }
+    obj[loop]++
+}
+
+console.log(obj)
