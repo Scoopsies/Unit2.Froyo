1bb45f4885ea412653a946e9ff5993eb7814b8a2
Scoopsies <mooninites103@gmail.com>
2023-08-20 20:34:25 -0500

    Did the problem over again, but made my own function to take the place of .split().

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-const input = window.prompt("Welcome to cone 'lickin, what ice cream you pickin", "vanilla,vanilla,vanilla,strawberry,coffee,coffee" );
-const flavArr = input.split(",");
-console.log(flavArr)
-let flavObj = {};
+const userInput = window.prompt("What flavors of Froyo do you want?", "vanilla,vanilla,vanilla,strawberry,coffee,coffee")
 
-
-for (let i = 0; i < flavArr.length; i++){
-    let loopArr = flavArr[i];
-    if (flavObj[loopArr] === undefined){
-        flavObj[loopArr] = 0;
+//does what .split(",") does, but i wanted to see if i could recreate it myself.
+function splitter(string){
+    let array = [];
+    let str = "";
+    for (i = 0; i < string.length; i++){
+        if (string[i] !== ","){
+            str += string[i];
+        }
+        if (string[i] === ","){
+            array.push(str);
+            str = '';
+        }
+        if (i === string.length - 1){
+            array.push(str)
+        }
+        
     }
-    flavObj[loopArr]++
+    return array;
 }
 
-console.log(flavObj)
+const strArray = splitter(userInput);
+
+
+//Creates an object that adds up each unique array item.
+
+function flavorObj(strArray){
+    const order = {};
+    for (i = 0; i < strArray.length; i++){
+        
+      let iteration = strArray[i]  
+
+      if (order[iteration] === undefined){
+        order[iteration] = 1;
+      } else {
+        order[iteration] += 1;
+      }
+    }
+    return order;
+}
+
+console.log(flavorObj(strArray))
+
+
+
